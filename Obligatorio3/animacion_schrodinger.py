0fa91a168b5940a5c21c62a4c814f29509f6b221
Antonio Marín Carballo <antoniomarin333@gmail.com>
2024-05-15 17:41:53 +0200

    Actualiacion + videos

--- Obligatorio3/animacion_schrodinger.py
+++ Obligatorio3/animacion_schrodinger.py
@@ -45,10 +45,10 @@ import io
 
 # Parámetros
 # ========================================
-file_in = "C:/Users/anton/OneDrive/Escritorio/A.MarinCarballoCompu2324/A.MarinCarballoCompu2324/Obligatorio3/Schrodinger.txt" # Nombre del fichero de datos
-file_out = "schrodinger" # Nombre del fichero de salida (sin extensión)
+file_in = "C:/Users/anton/OneDrive/Escritorio/A.MarinCarballoCompu2324/A.MarinCarballoCompu2324/Obligatorio3/Norma.txt" # Nombre del fichero de datos
+file_out = "NormaConservada" # Nombre del fichero de salida (sin extensión)
 interval = 100 # Tiempo entre fotogramas en milisegundos
-save_to_file = False # False: muestra la animación por pantalla,
+save_to_file = True # False: muestra la animación por pantalla,
                      # True: la guarda en un fichero
 dpi = 150 # Calidad del vídeo de salida (dots per inch)
 
